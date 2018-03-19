1c68a99fcec686b26006823e53155f73a1a96b19
Ralph <ralph@Ralphs-MBP.myralphnetrouter.net>
2018-03-19 17:18:14 +0400

    Adjustments

---
+++ arnev.js
@@ -0,0 +1,28 @@
+/*
+                                                        
+_____ _______  ____   _______  __   ____   ______ ______
+\__  \\_  __ \/    \_/ __ \  \/ / _/ ___\ /  ___//  ___/
+ / __ \|  | \/   |  \  ___/\   /  \  \___ \___ \ \___ \ 
+(____  /__|  |___|  /\___  >\_/ /\ \___  >____  >____  >
+     \/           \/     \/     \/     \/     \/     \/ 
+
+arnev.css - a lightweight web framework.
+
+File: arnev.js
+Purpose: functions to interact with arnev.css elements.
+
+(C) ASDT 2018.
+*/
+
+//Arnev Class
+var _Arnev = function()
+{
+    //Check if element is checked (id must be handle id)
+    this.SwitchIsChecked = function(hid)
+    {
+        var e = document.getElementById(hid);
+
+    }
+}
+
+var Arnev = new _Arnev();
